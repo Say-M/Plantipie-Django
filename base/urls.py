52ae42318614a535cda68391b178db76f7c1a120
Md-Raihan-Alam <mdraihanalam40@gmail.com>
2023-05-11 00:42:23 +0600

    Page restriction, login, authentic, logout, admin and user button added, login page and signup page updated.
    1. Page restriction has been added, after signing user/admin can not access login/signup page. No one will be able to access product details without signing in.
    2. Different dropdown has been added for user and admin.
    3. Login, authenticate, logout function in view has been created.

--- base/urls.py
+++ base/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('product/', views.product, name='product'),
     path('product/<str:id>/', views.product_detail, name='product_detail'),
-    path('login/', views.login, name='login'),
+    path('login', views.login_view, name='login'),
     path('signup/', views.signup, name='signup'),
+    path('logout',views.logout_user,name='logout')
 ]
--- base/views.py
+++ base/views.py
@@ -1,18 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'base/home.html', {'range': range(1, 13)})
+    return render(request, 'base/home.html', {'range': range(1, 13),'curUser':""})
 
 def product(request):
     return render(request, 'base/product.html', {'range': range(1, 13)})
 
+@login_required(login_url="/login")
 def product_detail(request, id):
     return render(request, 'base/product_detail.html', {'range': range(1, 5)})
 
-def login(request):
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == "POST":
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+        print(email)
+        print(password)
+        user=authenticate(request,username=email,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
     return render(request, 'base/auth/login.html')
 
+def logout_user(request):
+    logout(request)
+    return redirect('home')
+
 def signup(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == "POST":
+        email=request.POST.get("email")
+        firstname=request.POST.get("firstname")
+        lastname=request.POST.get("lastname")
+        password=request.POST.get("password")
+        my_user = User.objects.create_user(username=email, password=password, first_name=firstname, last_name=lastname)
+        my_user.save()
+        print("success")
+        return redirect("login")
     return render(request, 'base/auth/signup.html')
