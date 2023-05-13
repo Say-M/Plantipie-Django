48df96ecd95dbda91fc03b18cc755048ec14ffdd
unknown <msayem089@gmail.com>
2023-05-14 01:12:54 +0600

    edit

--- base/urls.py
+++ base/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('product/<str:id>/', views.product_detail, name='product_detail'),
     path('login', views.login_view, name='login'),
     path('signup/', views.signup, name='signup'),
-    path('logout',views.logout_user,name='logout')
+    path('logout',views.logout_user,name='logout'),
+    path('create-plant-item',views.create_plant_item,name="create-plant-item"),
 ]
 urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
